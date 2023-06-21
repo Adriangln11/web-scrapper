3d0056c17820e4fa8ae5f4447a8d2a0f84b19722
AdrianDiaz <cesarqqu@gmail.com>
2023-06-20 23:57:54 -0600

    html interface

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ class MyRequestHandler(BaseHTTPRequestHandler):
 def run(server_class=HTTPServer, handler_class=MyRequestHandler, port=8000):
     server_address = ('', port)
     httpd = server_class(server_address, handler_class)
-    print('Servidor en ejecución en el puerto', port)
+    print('Server listening on port: ', port)
     httpd.serve_forever()
 
 if __name__ == '__main__':
